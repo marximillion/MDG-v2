6eaaa6a2425794381a24806de834d7e39459e366
marximillion <marximillion@gmail.com>
2025-06-12 04:05:47 -0600

    [INSTALL] - Add assets

---
+++ app/assets/images.ts
@@ -0,0 +1,22 @@
+export const images = Object.freeze({
+
+  // Background Images
+  background_image: require('./imageAssets/backgroundImage.jpeg'),
+
+  // Car Images
+  csx: require('./imageAssets/noImage.png'),
+  csx_type_s: require('./imageAssets/noImage.png'),
+  crv: require('./imageAssets/noImage.png'),
+  odyssey: require('./imageAssets/noImage.png'),
+
+  // Random Images
+  clown: require('./imageAssets/clown.png'),
+
+  // Placeholder Images
+  no_image: require('./imageAssets/noImage.png'),
+
+  // JavaScript & jQuery images
+  check_cross: require('./imageAssets/check-cross.png'),
+  travelworthy_backdrop: require('./imageAssets/travelworthy-backdrop.jpg'),
+  travelworthy_logo: require('./imageAssets/travelworthy-logo.gif'),
+});
